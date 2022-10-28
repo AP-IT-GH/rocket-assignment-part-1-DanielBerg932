d62535122e4b60c0f4b120d6b4bbbc0c3c34d228
Daniel <danielberger932@gmail.com>
2022-10-28 14:37:56 +0200

    final commit

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -34,13 +34,21 @@ public class Move : MonoBehaviour
 
     public void MoveWithArrows()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             transform.Rotate(Vector3.forward * -15* Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             transform.Rotate(Vector3.forward * 15* Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * 5 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right *5 * Time.deltaTime);
         }
     }
 }
