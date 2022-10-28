f009e286179f839b1c3cda9dcf256ffb921b13f4
Daniel <danielberger932@gmail.com>
2022-10-28 14:28:18 +0200

    test2

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public int speed = 50;
-    public int sec = 20;
+    public int speed = 5;
 
     private Rigidbody myRb;
     // Start is called before the first frame update
@@ -28,9 +27,7 @@ public class Move : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-
-            transform.Translate(Vector3.up * speed * Time.deltaTime);
-            myRb.useGravity = false;
+            myRb.AddForce(Vector3.up * speed, ForceMode.Impulse);
         }
     }
 
@@ -39,11 +36,11 @@ public class Move : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector3.right * 5 * Time.deltaTime);
+            transform.Rotate(Vector3.forward * -15* Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(Vector3.left * 5 * Time.deltaTime);
+            transform.Rotate(Vector3.forward * 15* Time.deltaTime);
         }
     }
 }
